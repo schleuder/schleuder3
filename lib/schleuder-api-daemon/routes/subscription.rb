schleuder/schleuder

Convert configurable attributes of subscription to string

lib/schleuder-api-daemon/routes/subscription.rb
@@ -3,9 +3,9 @@ class SchleuderApiDaemon < Sinatra::Base
 
   namespace '/subscriptions' do
     get '.json' do
-      filterkeys = Subscription.configurable_attributes + [:list_id, :email]
+      filterkeys = subscriptions_controller.get_configurable_attributes + ['list_id', 'email']
       filter = params.select do |param|
-        filterkeys.include?(param.to_sym)
+        filterkeys.include?(param)
       end
 
       logger.debug "Subscription filter: #{filter.inspect}"

schleuder/schleuder3

(cherry picked from commit e81d9b4260d701aae3085d3d6dcf74694f1f9d42)

lib/schleuder-api-daemon/routes/subscription.rb
@@ -3,9 +3,9 @@ class SchleuderApiDaemon < Sinatra::Base
 
   namespace '/subscriptions' do
     get '.json' do
-      filterkeys = Subscription.configurable_attributes + [:list_id, :email]
+      filterkeys = Subscription.configurable_attributes + ['list_id', 'email']
       filter = params.select do |param|
-        filterkeys.include?(param.to_sym)
+        filterkeys.include?(param)
       end
 
       logger.debug "Subscription filter: #{filter.inspect}"